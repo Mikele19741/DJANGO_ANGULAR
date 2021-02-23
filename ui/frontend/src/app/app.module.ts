14515e2e3efbf081fbecbf7ab7334ab25693cac3
Mikhail Shilkov <shilkovmike@gmail.com>
2021-02-23 14:28:14 +0200

    added components

--- ui/frontend/src/app/app.module.ts
+++ ui/frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { ShowDepComponent } from './department/show-dep/show-dep.component';
+import { AddEditDepComponent } from './department/add-edit-dep/add-edit-dep.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DepartmentComponent,
+    EmployeesComponent,
+    ShowDepComponent,
+    AddEditDepComponent
   ],
   imports: [
     BrowserModule,
